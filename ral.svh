089c74c97154995c379d8bab30ea0d17a3a6dafa
huangniuniu <huangniuniu@gmail.com>
2015-06-14 20:52:57 +0800

    temp version
    stopped on writing adapter, reg2bus
    the register fields may need modified: let opcode be address offset and
    add o_dr_length to accord with jtag_transatction.

---
+++ ral.svh
@@ -0,0 +1,115 @@
+//------------------------------------------------------------------------------
+// Class: ieee1149_bypass_reg
+//------------------------------------------------------------------------------
+
+class ieee1149_bypass_reg extends uvm_reg;
+   `uvm_object_utils( ieee1149_bypass_reg )
+
+        uvm_reg_field opcode;
+   rand uvm_reg_field bypass;
+
+   function new( string name = "ieee1149_bypass_reg" );
+      super.new( .name( name ), .n_bits( `IR_WIDTH + 1 ), .has_coverage( UVM_NO_COVERAGE ) );
+   endfunction: new
+
+   virtual function void build();
+      opcode = uvm_reg_field::type_id::create( "opcode" );
+      opcode.configure( .parent                 ( this ), 
+                        .size                   ( `IR_WIDTH    ), 
+                        .lsb_pos                ( 0    ), 
+                        .access                 ( "RW" ), 
+                        .volatile               ( 0    ),
+                        .reset                  ( {`IR_WIDTH{1'b1}}    ), 
+                        .has_reset              ( 1    ), 
+                        .is_rand                ( 0    ), 
+                        .individually_accessible( 0    ) );
+
+      bypass = uvm_reg_field::type_id::create( "bypass" );
+      bypass.configure( .parent                 ( this ), 
+                       .size                   ( 1    ), 
+                       .lsb_pos                ( `IR_WIDTH    ), 
+                       .access                 ( "RW" ), 
+                       .volatile               ( 0    ),
+                       .reset                  ( 0    ), 
+                       .has_reset              ( 1    ), 
+                       .is_rand                ( 1    ), 
+                       .individually_accessible( 0   ) );
+
+   endfunction: build
+endclass: ieee1149_bypass_reg
+
+//------------------------------------------------------------------------------
+// Class: ieee1149_idcode_reg
+//------------------------------------------------------------------------------
+
+class ieee1149_idcode_reg extends uvm_reg;
+   `uvm_object_utils( ieee1149_idcode_reg )
+
+        uvm_reg_field opcode;
+   rand uvm_reg_field idcode;
+
+   function new( string name = "ieee1149_idcode_reg" );
+      super.new( .name( name ), .n_bits( `IR_WIDTH + 1 ), .has_coverage( UVM_NO_COVERAGE ) );
+   endfunction: new
+
+   virtual function void build();
+      opcode = uvm_reg_field::type_id::create( "opcode" );
+      opcode.configure( .parent                 ( this ), 
+                        .size                   ( `IR_WIDTH    ), 
+                        .lsb_pos                ( 0    ), 
+                        .access                 ( "RW" ), 
+                        .volatile               ( 0    ),
+                        .reset                  ( `IR_WIDTH'hF0    ), 
+                        .has_reset              ( 1    ), 
+                        .is_rand                ( 0    ), 
+                        .individually_accessible( 0    ) );
+
+      idcode = uvm_reg_field::type_id::create( "idcode" );
+      idcode.configure( .parent                 ( this ), 
+                       .size                   ( 8    ), 
+                       .lsb_pos                ( `IR_WIDTH    ), 
+                       .access                 ( "RW" ), 
+                       .volatile               ( 0    ),
+                       .reset                  ( 8'he3    ), 
+                       .has_reset              ( 1    ), 
+                       .is_rand                ( 1    ), 
+                       .individually_accessible( 0   ) );
+
+   endfunction: build
+endclass: ieee1149_idcode_reg
+
+//------------------------------------------------------------------------------
+// Class: ieee1149_1_reg_block
+//------------------------------------------------------------------------------
+
+class ieee1149_1_reg_block extends uvm_reg_block;
+   `uvm_object_utils( ieee1149_1_reg_block )
+
+   rand ieee1149_bypass_reg   bypass_reg;
+   rand ieee1149_idcode_reg   idcode_reg;
+   uvm_reg_map                reg_map;
+
+   function new( string name = "ieee1149_1_reg_block" );
+      super.new( .name( name ), .has_coverage( UVM_NO_COVERAGE ) );
+   endfunction: new
+
+   virtual function void build();
+      bypass_reg = ieee1149_bypass_reg::type_id::create( "bypass_reg" );
+      bypass_reg.configure( .blk_parent( this ) );
+      bypass_reg.build();
+
+      idcode_reg = ieee1149_idcode_reg::type_id::create( "idcode_reg" );
+      idcode_reg.configure( .blk_parent( this ) );
+      idcode_reg.build();
+
+      reg_map = create_map( .name( "reg_map" ), .base_addr( 8'h00 ), 
+                            .n_bytes( `MAX_N_BYTES ), .endian( UVM_LITTLE_ENDIAN ) );
+      reg_map.add_reg( .rg( bypass_reg ), .offset( 8'h00 ), .rights( "RW" ) );
+      reg_map.add_reg( .rg( idcode_reg  ), .offset( 8'h01 ), .rights( "RW" ) );
+      lock_model(); // finalize the address mapping
+   endfunction: build
+
+endclass: ieee1149_1_reg_block   
+
+
+
